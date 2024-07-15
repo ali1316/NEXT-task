9b2140a76a80320488fe6da097132c308ab3abf6
ali1316 <ali422004@gmail.com>
2024-07-15 18:20:52 +0300

    cccc

--- app/components/get_to_know_about.tsx
+++ app/components/get_to_know_about.tsx
@@ -12,7 +12,7 @@ export default function Get_to_know_about(){
                 <h1 className="text-bold text-5xl font-sans text-center text-black">Get to Know V.Connct Webinar</h1>
                 <p className="p-2 text-center text-lg mt-2">Explore the seamless flow of V.connct webinars, ensuring a smooth experience for both hosts and participants.</p>
             </div>
-        <div className="container m-auto h-auto grid grid-cols-3 gap-8 w-9/12 content-center">
+        <div className="container m-auto h-auto grid grid-cols-3 gap-8 w-9/12 content-center max-lg:grid-col-1 ">
             <div className="items-center justify-between gap-4 p-2" >
             <div className=" border border-black row-span-2 p-2 rounded-md">
                 <h2 className="text-bold text-3xl font-sans text-center	">Webinar Schedule</h2>
@@ -67,50 +67,52 @@ export default function Get_to_know_about(){
             </div>
         </div>
         </div>
-        <div className="container mx-auto text-white align-center text-center m-6">
+        <div className="container mx-auto text-white align-center text-center m-6 gap-6">
             <button className=" bg-[#1B1B1B] w-52 p-4 rounded-md" >Plans & Pricing</button>
         </div>
+
+
            {/*why choose variance*/}
-       <div className="text-white text-center  bg-black w-full  max-h-[836px]">
-            <div className=" grid grid-col-3 items-center justify-between gap-4">
-                <div className="col-span-1 border border-indigo w-18 h-16">
-                    <div className="row-span-1">
-                        <Image src={""} width={50} height={50}/>
-                        <h2>User-Friendly Interface</h2>
-                        <p>Navigate effortlessly through our user-friendly interface.</p>
-                    </div>
-                    <div className="row-span-1">
-                        <Image src={""} width={50} height={50}/>
-                        <h2>Live Stream Capabilities</h2>
-                        <p>Connect with participants globally in real-time with our live streaming option.</p>
-                    </div>
-                </div>
-                <div className="col-span-1 border border-indigo w-18 h-16">
-                    <div className="row-span-1">
-                        <Image src={""} width={50} height={50}/>
-                        <h2>Comprehensive Features</h2>
-                        <p>Enjoy a wide array of features. Tailor each webinar to your unique needs.</p>
-                    </div>
-                    <div className="row-span-1">
-                        <Image src={""} width={50} height={50}/>
-                        <h2>30-Hour Webinar Duration</h2>
-                        <p>Enjoy webinar durations for those in-depth discussions and comprehensive sessions.</p>
-                    </div>
-                </div>
-                <div className="col-span-1 border border-indigo w-18 h-16">
-                    <div className="row-span-1">
-                        <Image src={""} width={50} height={50}/>
-                        <h2>Up to 5000 Participants</h2>
-                        <p>Host anything from intimate seminars to grand conferences.</p>
-                    </div>
-                    <div className="row-span-1">
-                        <Image src={""} width={50} height={50}/>
-                        <h2>Cloud Recording Storage</h2>
-                        <p>Benefit from our cloud recording to ensuring you never miss a crucial detail.</p>
-                    </div>
-                </div>
-            </div>
+       <div className="text-white text-center mx-auto bg-black w-full  md:h-[836px] p-4 pt-8">
+           <h2 className="text-white text-6xl font-extrabold m-8 pt-8">Why Choose V.connct Webinar?</h2>
+           <div className="container mx-auto m-8">
+               <div className="grid grid-cols-3 max-sm:grid-cols-1 gap-4">
+                   <div className="border border-white p-7 m-3 rounded-lg">
+                       <Image src={"/pyr.png"} width={25} height={25}/>
+                       <h2>User-Friendly Interface</h2>
+                       <p>Navigate effortlessly through our user-friendly interface.</p>
+                   </div>
+                   <div className="border border-white p-7 m-3 rounded-lg">
+                       <Image src={"/like.png"} width={25} height={25}/>
+                       <h2>Comprehensive Features</h2>
+                       <p>Enjoy a wide array of features. Tailor each webinar to your unique needs.</p>
+                   </div>
+                   <div className="border border-white p-7 m-3 rounded-lg">
+                       <Image src={"/people.png"} width={25} height={25}/>
+                       <h2>Up to 2500 Participants</h2>
+                       <p>Host anything from intimate seminars to grand conferences.</p>
+                   </div>
+                   <div className="border border-white p-7 m-3 rounded-lg">
+                       <Image src={"/broadcast.png"} width={25} height={25}/>
+                       <h2>Live Stream Capabilities</h2>
+                       <p>Connect with participants globally in real-time with our live streaming option.</p>
+                   </div>
+                   <div className="border border-white p-7 m-3 rounded-lg">
+                       <Image src={"/clock.png"} width={25} height={25}/>
+                       <h2>30-Hour Webinar Duration</h2>
+                       <p>Enjoy webinar durations for those in-depth discussions and comprehensive sessions.</p>
+                   </div>
+                   <div className="border border-white p-7 m-3 rounded-lg">
+                       <Image src={"/cloud.png"} width={25} height={25}/>
+                       <h2>Cloud Recording Storage</h2>
+                       <p>Benefit from our cloud recording to ensuring you never miss a crucial detail.</p></div>
+
+               </div>
+               <div className="container mx-auto text-black align-center text-center m-6 gap-6">
+                   <button className=" bg-[#F8B517] w-52 p-4 rounded-md">Plans & Pricing</button>
+               </div>
+           </div>
        </div>
-    </>
-            )
+       </>
+    )
 }
